240007fb0b972692ce239d601654c4d294ff46a2
Jing Sun <sun.jing22@zte.com.cn>
2016-11-21 15:19:56 +0800

    add escalator frame
    JIRA:ESCALATOR-35
    
    This patch will support escalator service,and there is not real command can use.
    With this code, you can test with '/usr/bin/escalator-api' from command line.When
    service is up, you can use "curl http://127.0.0.1:19393" for verify the service.
    
    Change-Id: I5154328adf82ec70acb6e0ce12ef4b1701f7b710
    Signed-off-by: Jing Sun <sun.jing22@zte.com.cn>

---
+++ api/escalator/cmd/api.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+# Copyright 2010 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# Copyright 2011 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""
+Escalator API Server
+"""
+
+import os
+import sys
+
+import eventlet
+
+from oslo_service import systemd
+from oslo_config import cfg
+from oslo_log import log as logging
+import osprofiler.notifier
+import osprofiler.web
+
+from escalator.common import utils
+from escalator.common import config
+from escalator.common import exception
+from escalator.common import wsgi
+from escalator import notifier
+
+
+# Monkey patch socket, time, select, threads
+eventlet.patcher.monkey_patch(all=False, socket=True, time=True,
+                              select=True, thread=True, os=True)
+
+# If ../escalator/__init__.py exists, add ../ to Python search path, so that
+# it will override what happens to be installed in /usr/(local/)lib/python...
+possible_topdir = os.path.normpath(os.path.join(os.path.abspath(sys.argv[0]),
+                                   os.pardir,
+                                   os.pardir))
+if os.path.exists(os.path.join(possible_topdir, 'escalator', '__init__.py')):
+    sys.path.insert(0, possible_topdir)
+
+
+CONF = cfg.CONF
+CONF.import_group("profiler", "escalator.common.wsgi")
+logging.register_options(CONF)
+
+
+def fail(e):
+    sys.exit(100)
+
+
+def main():
+    try:
+        config.parse_args()
+        wsgi.set_eventlet_hub()
+        logging.setup(CONF, 'escalator')
+
+        if cfg.CONF.profiler.enabled:
+            _notifier = osprofiler.notifier.create("Messaging",
+                                                   notifier.messaging, {},
+                                                   notifier.get_transport(),
+                                                   "escalator", "api",
+                                                   cfg.CONF.bind_host)
+            osprofiler.notifier.set(_notifier)
+        else:
+            osprofiler.web.disable()
+
+        server = wsgi.Server()
+        server.start(config.load_paste_app('escalator-api'), default_port=9393)
+        systemd.notify_once()
+        server.wait()
+    except Exception as e:
+        fail(e)
+
+
+if __name__ == '__main__':
+    main()
---
+++ api/escalator/opts.py
@@ -0,0 +1,62 @@
+# Copyright (c) 2014 OpenStack Foundation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import copy
+import itertools
+
+import escalator.api.middleware.context
+import escalator.api.versions
+import escalator.common.config
+import escalator.common.rpc
+import escalator.common.wsgi
+import escalator.notifier
+
+__all__ = [
+    'list_api_opts',
+]
+
+
+_api_opts = [
+    (None, list(itertools.chain(
+        escalator.api.middleware.context.context_opts,
+        escalator.api.versions.versions_opts,
+        escalator.common.config.common_opts,
+        escalator.common.rpc.rpc_opts,
+        escalator.common.wsgi.bind_opts,
+        escalator.common.wsgi.eventlet_opts,
+        escalator.common.wsgi.socket_opts,
+        escalator.notifier.notifier_opts))),
+    ('task', escalator.common.config.task_opts),
+    ('paste_deploy', escalator.common.config.paste_deploy_opts)
+]
+
+
+def list_api_opts():
+    """Return a list of oslo_config options available in Escalator API service.
+
+    Each element of the list is a tuple. The first element is the name of the
+    group under which the list of elements in the second element will be
+    registered. A group name of None corresponds to the [DEFAULT] group in
+    config files.
+
+    This function is also discoverable via the 'escalator.api' entry point
+    under the 'oslo_config.opts' namespace.
+
+    The purpose of this is to allow tools like the Oslo sample config file
+    generator to discover the options exposed to users by escalator.
+
+    :returns: a list of (group_name, opts) tuples
+    """
+
+    return [(g, copy.deepcopy(o)) for g, o in _api_opts]
